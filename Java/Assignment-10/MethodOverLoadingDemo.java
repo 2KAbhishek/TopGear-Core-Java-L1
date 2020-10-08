18ecb6e6f409c2b15fad0b37d55fc2681728d39f
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 15:13:08 +0530

    Add MethodOverLoadingDemo.java

---
+++ Java/Assignment-10/MethodOverLoadingDemo.java
@@ -0,0 +1,7 @@
+public class MethodOverLoadingDemo {
+    public static void main(String[] args) {
+        MethodOverLoading mol = new MethodOverLoading();
+        System.out.println("Square of 5: " + mol.findSquare(5));
+        System.out.println("Square of 5.5: " + mol.findSquare(5.5));
+    }
+}
