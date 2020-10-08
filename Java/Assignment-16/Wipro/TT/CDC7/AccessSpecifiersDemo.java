de832465a26e3616e1bc2714863e63f049e8fb2c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 22:30:23 +0530

    Add AccessSpecifiersDemo.java

---
+++ Java/Assignment-16/Wipro/TT/CDC7/AccessSpecifiersDemo.java
@@ -0,0 +1,16 @@
+package Wipro.TT.CDC7;
+
+import Wipro.TT.ProjectZone.*;
+
+public class AccessSpecifiersDemo {
+    void view() {
+        AccessSpecifiers acc = new AccessSpecifiers();
+        acc.display();
+    }
+
+    public static void main(String a[]) {
+        AccessSpecifiersDemo object = new AccessSpecifiersDemo();
+        object.view();
+    }
+
+}
