052bd0225c7294d4b12837c51b30175f8603e5c5
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 15:13:08 +0530

    Add MethodOverLoading.java

---
+++ Java/Assignment-10/MethodOverLoading.java
@@ -0,0 +1,9 @@
+public class MethodOverLoading {
+    public int findSquare(int iNumber) {
+        return (iNumber * iNumber);
+    }
+
+    public double findSquare(double dNumber) {
+        return (dNumber * dNumber);
+    }
+}
