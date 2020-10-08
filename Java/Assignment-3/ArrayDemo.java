bcb877ca64f6448eaf2b76ae5ed4060ca163ff1c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 12:06:11 +0530

    Add ArrayDemo.java

---
+++ Java/Assignment-3/ArrayDemo.java
@@ -0,0 +1,7 @@
+public class ArrayDemo {
+    public static void main(String args[]) {
+        ArrayCreation objArrayCreation = new ArrayCreation();
+        objArrayCreation.arrayValues();
+        objArrayCreation.arrayDisplay();
+    }
+}
