955f1cea146c51ecde23d4fdc67d1b8013ab6f30
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 12:39:28 +0530

    Add RandomDemo.java

---
+++ Java/Assignment-5/RandomDemo.java
@@ -0,0 +1,8 @@
+public class RandomDemo {
+    public static void main(String[] args) {
+        RandomA rndA = new RandomA();
+        rndA.rndMsg();
+        RandomB rndB = new RandomB();
+        rndB.rndMsg();
+    }
+}
