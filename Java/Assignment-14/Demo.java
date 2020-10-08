e7e2f1766a5ae0b9fdec889d2febc218da37178c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 16:09:02 +0530

    Add Demo.java

---
+++ Java/Assignment-14/Demo.java
@@ -0,0 +1,11 @@
+abstract class Demo {
+    protected static int count;
+    private int number;
+
+    abstract void getValues();
+
+    final void displayValues() {
+    }
+    // final void compute(){ }
+    // (1)
+}
