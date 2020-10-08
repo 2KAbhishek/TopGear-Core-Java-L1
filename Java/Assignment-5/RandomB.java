dda6c9f3a3a5368f871b9c01d1f09a1301d15774
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 12:39:28 +0530

    Add RandomB.java

---
+++ Java/Assignment-5/RandomB.java
@@ -0,0 +1,6 @@
+public class RandomB {
+    void rndMsg() {
+        double val = Math.random();
+        System.out.println(val > 0.5 ? "The value is less than 0.5" : "The value is greater than 0.5");
+    }
+}
