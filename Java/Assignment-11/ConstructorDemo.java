e9cced150947cc6b1e3f1fc09e22e4552a5e6f87
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 15:13:14 +0530

    Add ConstructorDemo.java

---
+++ Java/Assignment-11/ConstructorDemo.java
@@ -0,0 +1,7 @@
+public class ConstructorDemo {
+    public static void main(String[] args) {
+        ConstructorOverload cv1 = new ConstructorOverload();
+        ConstructorOverload cv2 = new ConstructorOverload(1);
+        ConstructorOverload cv3 = new ConstructorOverload(2, 3);
+    }
+}
