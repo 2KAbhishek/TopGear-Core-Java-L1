f8aa3c4c387d734ff59c2a801d5e4f14423c9247
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 12:19:41 +0530

    Add Book.java

---
+++ Java/Assignment-4/Book.java
@@ -0,0 +1,20 @@
+public class Book {
+    String bookTitle;
+    float price;
+
+    public void setName(String bookName) {
+        bookTitle = bookName;
+    }
+
+    public void setPrice(float cost) {
+        price = cost;
+    }
+
+    public String getName() {
+        return bookTitle;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
